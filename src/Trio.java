e2a3c8ca45e9255be7651fb4d00d93bef130e8c4
parkerstr8 <43326040+parkerstr8@users.noreply.github.com>
2018-11-25 23:02:28 -0500

    done

--- src/Trio.java
+++ src/Trio.java
@@ -1,24 +1,33 @@
-public class Trio implements MenuItem
-{
+public class Trio implements MenuItem {
     private Sandwich sandwich;
     private Salad salad;
     private Drink drink;
-}
+
     public Trio(Sandwich sandwich, Salad salad, Drink drink) {
         this.sandwich = sandwich;
         this.salad = salad;
         this.drink = drink;
-        getName()
-        {
-            return this.sandwich.getName() + "/" + this.salad.getName+"/"
-                    +this.drink.getName+"Trio";
-        }
-        getPrice()
-        {
-            int x=this.sandwich.getPrice();
-            int y=this.salad.getPrice();
-            int z=this.drink.getPrice();
-        }
-
     }
+       public String getName()
+        {
+         return this.sandwich.getName() + "/" + this.salad.getName() + "/" + this.drink.getName() + " Trio";
+        }
+        public double getPrice()
+        {
+            double x = this.sandwich.getPrice();
+            double y = this.salad.getPrice();
+            double z = this.drink.getPrice();
 
+            if(x<=y && x<=z) {
+                return y + z;
+            }
+            if (x>=z)
+            {
+                return x+y;
+            }
+            return x+z;
+
+        }
+
+
+}
